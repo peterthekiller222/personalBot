89387669dad79a7ce960d016f05b5d7fd45159d6
peterthekiller222 <63990853+peterthekiller222@users.noreply.github.com>
2022-04-08 16:25:29 +1000

    V2

--- events/events/ready.js
+++ events/events/ready.js
@@ -3,7 +3,7 @@ const prefix = config.prefix;
 
 module.exports = client => {
     client.user.setActivity(`p help`, { type: 'LISTENING' });
-    client.guilds.cache.get('731274366519869580').channels.cache.get('875250361261256744').send('BOT IS ONLINE AND READY');
+    
 
     console.log('BOT IS ONLINE AND READY');
 }
